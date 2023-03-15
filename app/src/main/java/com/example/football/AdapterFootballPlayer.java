2405102e76d2320287896696700d0a4d9738b79a
MuhammadIshaqMaulana <lana@mhs.mdp.ac.id>
2023-03-15 09:15:40 +0700

    menambahkan ID

---
+++ app/src/main/java/com/example/football/AdapterFootballPlayer.java
@@ -0,0 +1,86 @@
+package com.example.football;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdapterFootballPlayer extends RecyclerView.Adapter<AdapterFootballPlayer.ViewHolderPlayer>{
+
+    private Context ctx;
+    private ArrayList arrID, arrNama, arrNomor, arrKlub;
+
+    public AdapterFootballPlayer(Context ctx, ArrayList arrID, ArrayList arrNama, ArrayList arrNomor, ArrayList arrKlub) {
+        this.ctx = ctx;
+        this.arrNama = arrNama;
+        this.arrNomor = arrNomor;
+        this.arrKlub = arrKlub;
+        this.arrID = arrID;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderPlayer onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View varView = LayoutInflater.from(ctx).inflate(R.layout.list_item_player, parent, false);
+        return new ViewHolderPlayer(varView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderPlayer holder, int position) {
+        holder.tvNama.setText(arrNama.get(position).toString());
+        holder.tvNomor.setText(arrNomor.get(position).toString());
+        holder.tvKlub.setText(arrKlub.get(position).toString());
+        holder.tvID.setText(arrID.get(position).toString());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return arrNama.size();
+    }
+
+    public class ViewHolderPlayer extends RecyclerView.ViewHolder{
+        private TextView tvID, tvNama, tvNomor, tvKlub;
+        public ViewHolderPlayer(@NonNull View itemView) {
+            super(itemView);
+            tvNama = itemView.findViewById(R.id.tv_nama);
+            tvNomor = itemView.findViewById(R.id.tv_nomor);
+            tvKlub = itemView.findViewById(R.id.tv_klub);
+            tvID = itemView.findViewById(R.id.tv_id);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder pesan = new AlertDialog.Builder(ctx);
+                    pesan.setTitle("Perhatian");
+                    pesan.setMessage("Pilih perintah yang anda inginkan");
+                    pesan.setCancelable(true);
+
+                    pesan.setPositiveButton("ubah", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    pesan.setNegativeButton("Hapus", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+                    pesan.show();
+                    return false;
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/com/example/football/MainActivity.java
+++ app/src/main/java/com/example/football/MainActivity.java
@@ -1,23 +1,33 @@
 package com.example.football;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    MyDatabaseHelper myDB;
+    private MyDatabaseHelper myDB;
     private FloatingActionButton fabTambah;
+    private RecyclerView rvPlayer;
+    private AdapterFootballPlayer adPlayer;
+    private ArrayList<String> arrID, arrNama, arrNomor, arrKlub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        rvPlayer = findViewById(R.id.rv_player);
         myDB = new MyDatabaseHelper(MainActivity.this);
 
         fabTambah = findViewById(R.id.fab_tambah);
@@ -28,4 +38,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tampilPlayer();
+    }
+
+    private void SQLiteToArrayList(){
+        Cursor varCursor = myDB.bacaDataPlayer();
+        if(varCursor.getCount() == 0){
+            Toast.makeText(this, "Tidak Ada Data", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            while(varCursor.moveToNext()){
+                arrID.add(varCursor.getString(0));
+                arrNama.add(varCursor.getString(1));
+                arrNomor.add(varCursor.getString(2));
+                arrKlub.add(varCursor.getString(3));
+            }
+        }
+    }
+
+    private void tampilPlayer(){
+        arrID = new ArrayList<>();
+        arrNama = new ArrayList<>();
+        arrNomor = new ArrayList<>();
+        arrKlub = new ArrayList<>();
+
+        SQLiteToArrayList();
+
+        adPlayer = new AdapterFootballPlayer(MainActivity.this, arrID, arrNama, arrNomor, arrKlub);
+
+        rvPlayer.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        rvPlayer.setAdapter(adPlayer);
+    }
+
 }
